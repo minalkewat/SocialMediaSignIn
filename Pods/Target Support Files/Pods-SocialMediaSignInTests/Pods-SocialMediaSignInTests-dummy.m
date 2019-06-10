2a77b8726cf757df30be18f7b9e2a411fd19da78
minalkewat <minalkewat@gmail.com>
2019-06-10 21:30:42 +0530

    initial commit

---
+++ Pods/Target Support Files/Pods-SocialMediaSignInTests/Pods-SocialMediaSignInTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SocialMediaSignInTests : NSObject
+@end
+@implementation PodsDummy_Pods_SocialMediaSignInTests
+@end
---
+++ Pods/Target Support Files/Pods-SocialMediaSignInUITests/Pods-SocialMediaSignInUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SocialMediaSignInUITests : NSObject
+@end
+@implementation PodsDummy_Pods_SocialMediaSignInUITests
+@end
