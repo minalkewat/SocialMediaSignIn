4bf79a38720fc7af9b260901f274838048a4b55d
minalkewat <minalkewat@gmail.com>
2019-06-10 18:24:28 +0530

    Initial Commit

---
+++ SocialMediaSignIn/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SocialMediaSignIn
+//
+//  Created by Meenal Kewat on 6/10/19.
+//  Copyright © 2019 Meenal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
